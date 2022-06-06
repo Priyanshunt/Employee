0ed1f137444bfef4e11d7d97e32e656f5c6b2222
Priyanshu <priyanshuagg08@gmail.com>
2022-06-06 20:45:38 +0530

    Changes to solve deployment errors

--- src/main/java/com/springboot/demo/account/AccountApplication.java
+++ src/main/java/com/springboot/demo/account/AccountApplication.java
@@ -2,12 +2,12 @@ package com.springboot.demo.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AccountApplication {
+public class AccountApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
-
 }
